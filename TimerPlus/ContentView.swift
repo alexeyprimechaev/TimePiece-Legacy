9a512b3b30bf76621a2c34440e8cd7ac4f831ee3
алёша <aprimechaev@icloud.com>
2020-01-05 22:47:44 +0300

    Initial Commit

---
+++ TimerPlus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimerPlus
+//
+//  Created by Alexey Primechaev on 1/5/20.
+//  Copyright © 2020 Alexey Primechaev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
