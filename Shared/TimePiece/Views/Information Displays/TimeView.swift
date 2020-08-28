3f0ee8011d437dea152f062ebcab5d9c3976bc08
алёша <asprimechaev@icloud.com>
2020-08-28 17:27:04 +0300

    работаем!

--- Shared/TimePiece/Views/Information Displays/TimeView.swift
+++ Shared/TimePiece/Views/Information Displays/TimeView.swift
@@ -20,114 +20,168 @@ struct TimeView: View {
     
     @State var string = ""
     
+    
+    @State var seconds = ""
+    @State var minutes = ""
+    @State var hours = ""
+    
     var body: some View {
-        ZStack(alignment: .topLeading) {
-            HStack(spacing: 0) {
-                Text(timeString.count > 4 ? timeString.count > 5 ? timeString.prefix(2) : timeString.prefix(1) : "")
-                    .frame(width:46, alignment: .topTrailing)
-                    .opacity(keyboardMode == 1 ? 1 : 0.5)
+        VStack() {
+            ZStack(alignment: .topLeading) {
+                HStack(spacing: 0) {
+                    Text(hours)
+                        .frame(width:46, alignment: .topTrailing)
+                        .opacity(keyboardMode == 1 ? 1 : 0.5)
 
-                Dots()
-                Text(timeString.count > 2 ? timeString.count > 3 ? timeString.suffix(4).prefix(2) : timeString.suffix(3).prefix(1) : "")
-                    .frame(width:46, alignment: .topTrailing)
-                    .opacity(keyboardMode == 2 ? 1 : 0.5)
-                Dots()
-                Text(timeString.count > 1 ? timeString.suffix(2) : timeString.suffix(1))
-                    .frame(width:46, alignment: .topTrailing)
-                    .opacity(keyboardMode == 3 ? 1 : 0.5)
-            }.title()
-            .overlay(
-                TextField("", text: $string) {
-                    while timeString.count < 6 {
-                        timeString = "0" + timeString
+                    Dots()
+                    Text(minutes)
+                        .frame(width:46, alignment: .topTrailing)
+                        .opacity(keyboardMode == 2 ? 1 : 0.5)
+                    Dots()
+                    Text(seconds)
+                        .frame(width:46, alignment: .topTrailing)
+                        .opacity(keyboardMode == 3 ? 1 : 0.5)
+                }.title()
+                .overlay(
+                    TextField("", text: $string) {
+                        fixNumbers()
                     }
-                }
-                    .introspectTextField { textField in
-                        self.textField = textField
+                        .introspectTextField { textField in
+                            self.textField = textField
 
+                        }
+                        .title()
+                        .accentColor(.clear)
+                        .foregroundColor(.clear)
+                        .onTapGesture {
+                            timeString = ""
+                        }
+                        .onChange(of: string) { newValue in
+                            
+                            switch keyboardMode {
+                            case 1:
+                                if newValue.count > 2 {
+                                    string = String(newValue.suffix(2))
+                                    hours = string
+                                } else {
+                                    hours = newValue
+                                }
+                                timeString = hours + minutes + seconds
+                            case 2:
+                                if newValue.count > 2 {
+                                    string = String(newValue.suffix(2))
+                                    minutes = string
+                                } else {
+                                    minutes = newValue
+                                }
+                                timeString = hours + minutes + seconds
+                            case 3:
+                                if newValue.count > 2 {
+                                    string = String(newValue.suffix(2))
+                                    seconds = string
+                                } else {
+                                    seconds = newValue
+                                }
+                                timeString = hours + minutes + seconds
+                            default:
+                                if newValue.count > 6 {
+                                    timeString = String(newValue.suffix(6))
+                                } else {
+                                    timeString = newValue
+                                }
+                            }
+                            
+                            
+                            
+                            print(string)
+                            print(timeString)
+                        }
+                    .onAppear {
+                        hours = String(timeString.prefix(2))
+                        minutes = String(timeString.prefix(4).suffix(2))
+                        seconds = String(timeString.suffix(2))
                     }
-                    .title()
-                    .accentColor(.clear)
-                    .foregroundColor(.clear)
-                    .onTapGesture {
-                        timeString = ""
-                    }
-                    .onChange(of: string) { newValue in
+                        .keyboardType(.numberPad)
                         
-                        switch keyboardMode {
-                        case 1:
-                            if newValue.count > 2 {
-                                string = String(newValue.suffix(2))
-                                timeString = String(newValue.suffix(6))
-                            } else {
-                                timeString = newValue + timeString.suffix(4)
-                            }
-                        case 2:
-                            if newValue.count > 2 {
-                                string = String(newValue.suffix(2))
-                                timeString = timeString.prefix(2) + newValue + timeString.suffix(2)
-                            } else {
-                                timeString = timeString.prefix(2) + newValue + timeString.suffix(2)
-                            }
-                        case 3:
-                            if newValue.count > 2 {
-                                string = String(newValue.suffix(2))
-                                timeString = timeString.prefix(4) + newValue
-                            } else {
-                                timeString = timeString.prefix(4) + newValue
-                            }
-                        default:
-                            if newValue.count > 6 {
-                                timeString = String(newValue.suffix(6))
-                            } else {
-                                timeString = newValue
-                            }
+                )
+    //            .background(HStack(spacing: 0) {
+    //                Text("00")
+    //                    .frame(width:46, alignment: .topTrailing)
+    //                Dots().opacity(0)
+    //                Text("00")
+    //                    .frame(width:46, alignment: .topTrailing)
+    //                Dots().opacity(0)
+    //                Text("00")
+    //            }.opacity(timeString.count == 0 ? 0.5 : 0).title())
+                HStack(spacing: 0) {
+                    Button(action: {
+                        if textField.isEditing {
+                            keyboardMode = 1
+                            print(keyboardMode)
+                            fixNumbers()
+                            string = ""
+                        } else {
+                            keyboardMode = 0
+                        }
+                        textField.becomeFirstResponder()
+                        
+                    }) {
+                        Rectangle().frame(width: 50, height: 36).opacity(0)
+                    }
+                    Button(action: {
+                        if textField.isEditing {
+                            keyboardMode = 2
+                            fixNumbers()
+                            print(keyboardMode)
+                            string = ""
+                        } else {
+                            keyboardMode = 0
+                        }
+                        textField.becomeFirstResponder()
+                    }) {
+                        Rectangle().frame(width: 50, height: 36).opacity(0)
+                    }
+                    Button(action: {
+                        if textField.isEditing {
+                            keyboardMode = 3
+                            fixNumbers()
+                            print(keyboardMode)
+                            string = ""
+                        } else {
+                            keyboardMode = 0
                         }
                         
-                        
-                        
-                        print(string)
-                        print(timeString)
+                        textField.becomeFirstResponder()
+                    }) {
+                        Rectangle().frame(width: 50, height: 36).opacity(0)
                     }
-                    .keyboardType(.numberPad)
-                    
-            )
-//            .background(HStack(spacing: 0) {
-//                Text("00")
-//                    .frame(width:46, alignment: .topTrailing)
-//                Dots().opacity(0)
-//                Text("00")
-//                    .frame(width:46, alignment: .topTrailing)
-//                Dots().opacity(0)
-//                Text("00")
-//            }.opacity(timeString.count == 0 ? 0.5 : 0).title())
-            HStack(spacing: 0) {
-                Button(action: {
-                    keyboardMode = 1
-                    print(keyboardMode)
-                    textField.becomeFirstResponder()
-                }) {
-                    Rectangle().frame(width: 50, height: 36).opacity(0)
-                }
-                Button(action: {
-                    keyboardMode = 2
-                    print(keyboardMode)
-                    textField.becomeFirstResponder()
-                }) {
-                    Rectangle().frame(width: 50, height: 36).opacity(0)
-                }
-                Button(action: {
-                    keyboardMode = 3
-                    print(keyboardMode)
-                    textField.becomeFirstResponder()
-                }) {
-                    Rectangle().frame(width: 50, height: 36).opacity(0)
                 }
             }
+            
+            Text("timeString: \(timeString)")
+            Text("hours: \(hours)")
+            Text("minutes: \(minutes)")
+            Text("seconds: \(seconds)")
+            Text("string: \(string)")
+            Text("keyboardMode: \(keyboardMode)")
+            
         }
         
         
         
+        
+    }
+    
+    func fixNumbers() {
+        while hours.count < 2 {
+            hours = "0" + hours
+        }
+        while minutes.count < 2 {
+            minutes = "0" + minutes
+        }
+        while seconds.count < 2 {
+            seconds = "0" + seconds
+        }
     }
     
 }
