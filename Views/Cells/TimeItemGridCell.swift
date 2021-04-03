465a012a54cd660c6a974d755d44d69df77928b0
алёша <asprimechaev@icloud.com>
2021-04-03 15:36:45 +0300

    мяу

--- Views/Cells/TimeItemGridCell.swift
+++ Views/Cells/TimeItemGridCell.swift
@@ -142,7 +142,6 @@ struct TimeItemGridCell: View {
                     .overlay(
                         RoundedRectangle(cornerRadius: 16, style: .continuous)
                             .stroke(Color.primary, lineWidth: 2)
-                            .foregroundColor(Color("button.gray"))
                             .opacity(timeItem == appState.selectedTimeItem && horizontalSizeClass != .compact ? 1 : 0))
                 .overlay(appState.isInEditing ? Image(systemName: appState.selectedValues.contains(timeItem) ? "checkmark.circle.fill" : "circle").font(.title2).padding(7) : nil, alignment: .topTrailing)
                     
